3d0a3b5095a7825a1134ad84e6581fd3ec6bb5d0
jdmmit <juandavidmurillomejia0718@outlook.es>
2025-06-25 17:33:57 -0500

    25/06/2025 01

--- Ejercicio_02.py
+++ Ejercicio_02.py
@@ -33,29 +33,75 @@
  
  """
 
-a = int(input("Ingresa un numero: "))
-b = 1234567890
+# Función sin parámetros ni retorno
+def funcion_sin_parametros():
+    print("Función sin parámetros ni retorno")
 
+# Función con un parámetro
+def funcion_con_parametro(nombre):
+    print(f"Hola, {nombre}")
 
-def funcion_global():
-    
-    for a in b:
-      if a == a:
-         print(a)
-      else:
-         print("Intenta otro numero")      
+# Función con varios parámetros y con retorno
+def funcion_con_retorno(a, b):
+    return a + b
 
-funcion_global()
-       
+# Función dentro de otra función
+def funcion_externa():
+    print("Estoy en la función externa")
 
+    def funcion_interna():
+        print("Estoy en la función interna")
 
+    funcion_interna()
 
+# Variables global y local
+x = 10  # Variable global
 
+def mostrar_variables():
+    x = 5  # Variable local
+    print("Variable local x:", x)
 
-"""for i in range(1, 10):
-   if i % 3 != 0 and i % 5 != 0:
-      print(i)
-         """
+# Función con dificultad extra: FizzBuzz personalizado
+def fizzbuzz(palabra1, palabra2):
+    contador_numeros = 0
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print(palabra1 + palabra2)
+        elif i % 3 == 0:
+            print(palabra1)
+        elif i % 5 == 0:
+            print(palabra2)
+        else:
+            print(i)
+            contador_numeros += 1
+    return contador_numeros
+
+# --- Ejecución y pruebas ---
+
+# Llamamos a la función sin parámetros ni retorno
+funcion_sin_parametros()
+
+# Llamamos a la función con parámetro
+funcion_con_parametro("Juan")
+
+# Llamamos a la función con retorno y mostramos el resultado
+resultado_suma = funcion_con_retorno(3, 7)
+print("Resultado de la suma:", resultado_suma)
+
+# Llamamos a la función externa que contiene otra función
+funcion_externa()
+
+# Mostramos variable local y global
+mostrar_variables()
+print("Variable global x:", x)
+
+# Usamos una función ya creada en Python: len()
+texto = "Hola"
+print("Longitud de texto:", len(texto))
+
+# Ejecutamos la función fizzbuzz y mostramos cuántas veces se imprimió un número
+veces_numeros = fizzbuzz("Fizz", "Buzz")
+print("Se imprimieron números", veces_numeros, "veces")
 
 
 
